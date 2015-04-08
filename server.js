de87337d133642016ee23c91c4e5a9a99d480a94
Felipe <FMurillo87@gmail.com>
2015-04-08 22:25:21 +0200

    asd

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
+var config = require('./config/config');
+var chalk = require('chalk');
+var mongoose = require('mongoose');
+var Juego = require('./server/modelos/juego');
 
-app.get("/", function(req, res) {
-	console.log("Peticion get a raiz");
-	res.send("Oleis a culo de mono");
-})
+var db = mongoose.connect(config.db.uri, function(err) {
+	if (err) {
+		console.error(chalk.red('Could not connect to MongoDB!'));
+		console.log(chalk.red(err));
+	}
+});
+mongoose.connection.on('error', function(err) {
+	console.error(chalk.red('MongoDB connection error: ' + err));
+	process.exit(-1);
+	}
+);
 
-app.listen(8080);
-console.log("Escuchando");
+require('./server/routers/index.router')(app);
+require('./server/routers/juegos.router')(app);
+
+app.listen(config.port);
+console.log("Escuchando en el puerto " + config.port);
