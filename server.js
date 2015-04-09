2cb7e122cd107a4f0f771ff39471525b074a1500
unknown <fmurillo@W7FMURILLO.sia.es>
2015-04-09 09:14:10 +0200

    server actual

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
+var config = require('./config/config');
+var mongoose = require('mongoose');
+require('./server/modelos/juego');
+var chalk = require('chalk');
 
-app.get("/", function(req, res) {
-	console.log("Peticion get a raiz");
-	res.send("Oleis a culo de mono");
-})
+var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
+	if (err) {
+		console.error(chalk.red("No se pudo establecer conexion con la base de datos"));
+		console.log(chalk.red(err));
+		process.exit(-1);
+	}
+});
+mongoose.connection.on('error', function(err) {
+	console.error(chalk.red("Error de conexion en base de datos: " + err));
+	process.exit(-1);
+	}
+);
 
-app.listen(8080);
-console.log("Escuchando");
+require('./server/routers/juegos.router')(app);
+require('./server/routers/index.router')(app);
+
+app.listen(config.port);
+console.log(chalk.green("Escuchando en el puerto " + config.port));
