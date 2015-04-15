7f7fd78b6b55cadc5fdc12f446a324e2771fa075
Felipe <FMurillo87@gmail.com>
2015-04-15 11:12:22 +0200

    vista de juego

--- public/main.js
+++ public/main.js
@@ -1,46 +1,42 @@
-var app = angular.module('igdbAngular', ['ngRoute']);  
+var app = angular.module('igdbAngular', ['ui.router']);  
 
-app.controller('gameController', function ($scope, $http, $location) {
+app.controller('gameController', function ($scope, $rootScope, $http, $location) {
 	$scope.formData = {};
 	
 	$http.get('/latest')
-		.success(function(games) {
-			$scope.games = games;
-			console.log(games);
+	.success(function(games) {
+		$scope.games = games;
+	})
+	.error(function(err) {
+		console.log(err);
+	});
+	
+	$scope.getGameById = function (id) {
+		$http.get('/games/' + id)
+		.success(function(game) {
+			$rootScope.game = game;
+			$location.path('games/' + id);
+
 		})
 		.error(function(err) {
 			console.log(err);
 		});
+	};
 
-	$scope.getGameById = function (id) {
-		console.log('Peticion a juego ' + id);
-		$http.get('/games/' + id)
-			.success(function(game) {
-				$scope.game = game;
-				$location.path('games/' + id);
-
-			})
-			.error(function(err) {
-				console.log(err);
-			})
-	}
-
-
-	
 	
 });
 
-var url = "http://localhost:8888/games/20960";
-	
-	//Al ejecutar el script, espera a que se pulse el boton para acceder al numero de visitas
-	$('document').ready( function() {
+app.config(function($stateProvider, $urlRouterProvider) {
 
-		$('#boton').click(function () {
-			console.log("boton pulsado");
-			$.ajax(url, {success: function(game) {
-				console.log(game);
-				$('#texto').html(game.gameTitle);
-			}});
-		});
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+	.state('home', {
+			url: '/',
+			templateUrl: 'main.html',
+		})
+	.state('game', {
+		url: '/games/:id',
+		templateUrl: 'game.html'
 	});
-	
+});
